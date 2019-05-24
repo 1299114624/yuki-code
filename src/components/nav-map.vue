1c305d1e07942befb311bc632bf540bab17a983f
1299114624 <1299114624@qq.com>
2019-05-24 23:08:11 +0800

    save

--- src/components/nav-map.vue
+++ src/components/nav-map.vue
@@ -1,10 +1,20 @@
 <template>
     <div>
         <h1>
-            本应放到开始里面,但先在这儿说明
+            本应搞个使用说明,但先在此简单说下怎么用
         </h1>
         <div>
-            
+            vue和yuki的js均通过cdn引入,index.html里引入js
+        </div>
+        <div>https://cdn.bootcss.com/vue/2.6.10/vue.js</div>
+        <div>https://cdn.jsdelivr.net/gh/1299114624/yuki@v1.1/dist/static/js/yuki.js</div>
+        <div>index.html里引入css</div>
+        <div>https://cdn.jsdelivr.net/gh/1299114624/yuki@v1.1/dist/static/css/yuki.css</div>
+        <div>
+            在vue2.0项目中build目录下的webpack.base.conf.js里加上:externals: {vue: 'window.Vue'},再运行项目即可
+        </div>
+        <div>目前只做了一个button按钮,使用说明详见左侧菜单栏</div>
+        <div>目前字体图标打包路径有点问题
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// import App from './App'
-// import router from './router'
-// import store from './store'
+import App from './App'
+import router from './router'
+import store from './store'
 import ElementUI from 'element-ui'
-// import DemoBlock from './base/demo-block'
-// // import SideNav from './components/side-nav'
+import DemoBlock from './base/demo-block'
+import SideNav from './components/side-nav'
 import 'element-ui/lib/theme-chalk/index.css'
 import './packages/theme-default/index.css'
 Vue.use(ElementUI)
-// Vue.component('demo-block',DemoBlock)
-// Vue.component('side-nav',SideNav)
+Vue.component('demo-block',DemoBlock)
+Vue.component('side-nav',SideNav)
 
 import YukiButton from './packages/button/src/main'
 const components = [YukiButton]
@@ -35,8 +35,8 @@ export default {
 // }).$mount('#app')
 
 
-// new Vue({
-//   router,
-//   store,
-//   render: h => h(App)
-// }).$mount('#app')
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
